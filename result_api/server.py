c5ac2cc4226b1aab49c106e636cf6975e4089e31
pilijevski <petar.ilijevski@gmail.com>
2022-08-18 12:51:22 +0200

    hotfix data not found (#47)

--- result_api/server.py
+++ result_api/server.py
@@ -1,11 +1,13 @@
 import logging.config
+from http import HTTPStatus
 
 from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 
+from responses.responses import ErrorResponse
 from result_api.dto import VerificationDTO
-from result_api.log_verification.verification import LogVerification
+from result_api.log_verification.verification import LogVerification, NotFoundException
 from logsight.services.service_provider import ServiceProvider
 from logsight.logger.configuration import LogConfig
 
@@ -18,9 +20,13 @@ logger = logging.getLogger('logsight')
 
 @app.post('/api/v1/compare')
 def get_tasks(request: VerificationDTO):
-    result = LogVerification(ServiceProvider.provide_elasticsearch()).run_verification(
-        private_key=request.privateKey,
-        baseline_tags=request.baselineTags,
-        candidate_tags=request.candidateTags)
+    try:
+        result = LogVerification(ServiceProvider.provide_elasticsearch()).run_verification(
+            private_key=request.privateKey,
+            baseline_tags=request.baselineTags,
+            candidate_tags=request.candidateTags)
+    except NotFoundException as e:
+        return JSONResponse(content=ErrorResponse(str(e)),
+                            status_code=HTTPStatus.NOT_FOUND)
     json_compatible_item_data = jsonable_encoder(result)
     return JSONResponse(content=json_compatible_item_data)
