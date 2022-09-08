b448e1067705f8fed5483888e7d85961fe798249
snedelkoski <sashonedelkoski@gmail.com>
2022-09-08 11:52:52 +0200

    link

--- result_api/log_verification/lambdas.py
+++ result_api/log_verification/lambdas.py
@@ -68,8 +68,8 @@ def get_code_link(template):
 
 def get_payment_risk(template, risk):
     if "Received payment response" in str(template):
-        return 50
-    elif "Sending payment authorization response" in str(template):
         return 55
+    elif "Sending payment authorization response" in str(template):
+        return 50
     else:
         return risk
